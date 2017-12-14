368af4c8e7a2299479a19cc3188269f88f9c66a2
Aragaki Yui <duke00xls@hotmail.com>
2017-12-15 04:25:45 +0800

    test source 5

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -2,7 +2,7 @@
 # This file execute all the tests contained in the folders
 #################################################################################
 library(testthat)
-sourceEntireFolder('R')
+#sourceEntireFolder('R')
 
 test_dir('tests', filter = NULL, reporter = "summary", env = test_env())
 
