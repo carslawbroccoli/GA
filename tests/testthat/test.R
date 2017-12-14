4198710a1266e5be0ac61ed7e87eeb721cd64530
Jeffrey Kwarsick <jeffrey.kwarsick@gmail.com>
2017-12-13 23:52:15 -0800

    fixing file source

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -2,7 +2,7 @@
 # This file execute all the tests contained in the folders
 #################################################################################
 library(testthat)
-source('./GA/R/utils.R')
+source('~/GA/R/utils.R')
 sourceEntireFolder('R')
 
 test_dir('tests', filter = NULL, reporter = "summary", env = test_env())
