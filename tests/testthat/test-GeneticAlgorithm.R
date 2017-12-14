1b2891f3ea1f0ae438ea85fd8c5306430e073391
Aragaki Yui <duke00xls@hotmail.com>
2017-12-14 18:30:20 +0800

    Aragaki Yuiiiiii

--- tests/testthat/test-GeneticAlgorithm.R
+++ tests/testthat/test-GeneticAlgorithm.R
@@ -215,10 +215,3 @@ test_that("when mechanism is 'rank', set 'random=FALSE' may converge to the same
                       random = TRUE, Gap = 1/4, plot.return = F)
   expect_equal(result11$fitness_value, result12$fitness_value)
 })
-
-test_that("select() can generate a plot when setting plot.return=TRUE", {
-  library(ggplot2)
-  result1<- select(mtcars, "mpg", 15, 200, "lm", "AIC", 0.2, 3, mechanism = "rank",
-                   random = FALSE, Gap = 1/4, plot.return = T)
-  expect_true(identical(class(result1$plot), c("gg", "ggplot")))
-})
